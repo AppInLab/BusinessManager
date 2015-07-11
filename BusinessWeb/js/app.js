32db9610511f90f36f943f66153c0d200d0e437b
willf80 <willyfkouadio@gmail.com>
2015-07-11 16:33:17 +0000

    finalisation pages

--- BusinessWeb/js/app.js
+++ BusinessWeb/js/app.js
@@ -1,30 +1,71 @@
-var honey = angular.module('poissonnerie', ['MainController', 'ngRoute']);
+var app = angular.module('poissonnerie',
+    [
+        'MainController',
+        'ngRoute',
+        'angularUtils.directives.dirPagination',
+        'angular-loading-bar',
+        'ngCookies',
+        'bootstrap-tagsinput',
+        'angular.filter',
+        'ngTagsInput'
+    ]
+    );
 
-honey.config(["$routeProvider",
-        function($routeProvider){
-            
+app.config(["$routeProvider",
+        function ($routeProvider) {
+
             //Acueil            
             $routeProvider.when("/home", {
                 templateUrl : "views/home.html",
                 controller: "HomeController"
-                })
-            
-            //Comptes Clients           
-            .when("/page1", {
-                templateUrl: "views/page1.html",
-                controller: "ContenuController"               
             })
 
-            //Badges
-            .when("/badges/:numAbonnement", {
-                templateUrl: "views/Badges/badges.html",
-                controller: "ContenuController"               
+            //Achat rapide            
+            $routeProvider.when("/AchatRapide", {
+                templateUrl: "views/AchatRapide.html",
+                controller: "AchatRapideController"
             })
 
-            //Administration
-            .when("/administration", {
-                templateUrl: "views/Administration/administration.html",
-                controller: "ContenuController"
+            //Produits            
+            $routeProvider.when("/Produits", {
+                templateUrl: "views/Produits.html",
+                controller: "ProduitsController"
+            })
+
+            //Clients            
+            $routeProvider.when("/Clients", {
+                templateUrl: "views/Clients.html",
+                controller: "ClientsController"
+            })
+
+            //Chineurs            
+            $routeProvider.when("/Chineurs", {
+                templateUrl: "views/Chineurs.html",
+                controller: "ChineursController"
+            })
+
+            //Fournisseurs            
+            $routeProvider.when("/Fournisseurs", {
+                templateUrl: "views/Fournisseurs.html",
+                controller: "FournisseursController"
+            })
+
+            //Fournisseurs            
+            $routeProvider.when("/BonLivraison", {
+                templateUrl: "views/BonLivraison.html",
+                controller: "BonLivraisonController"
+            })
+
+            //PointJournee            
+            $routeProvider.when("/PointJour", {
+                templateUrl: "views/PointJour.html",
+                controller: "PointJourController"
+            })
+
+            //Depenses            
+            $routeProvider.when("/Depenses", {
+                templateUrl: "views/Depenses.html",
+                controller: "DepensesController"
             })
 
             //Login
@@ -33,9 +74,90 @@ honey.config(["$routeProvider",
                 controller: "LoginController"
             })
 
+            /* Menus Administrateurs */
+
+            //Produits et catégories de produits            
+            $routeProvider.when("/AdminProduits", {
+                templateUrl: "views/Admin/AdminProduits.html",
+                controller: "AdminProduitsController"
+            })
+
+            //Gestion des fornisseurs      
+            $routeProvider.when("/AdminFournisseurs", {
+                templateUrl: "views/Admin/AdminFournisseurs.html",
+                controller: "AdminFournisseursController"
+            })
+
+            //Gestion des Chineurs      
+            $routeProvider.when("/AdminChineurs", {
+                templateUrl: "views/Admin/AdminChineurs.html",
+                controller: "AdminChineursController"
+            })
+
+            //Gestion des Utilisateurs      
+            $routeProvider.when("/AdminUtilisateurs", {
+                templateUrl: "views/Admin/AdminUtilisateurs.html",
+                controller: "AdminUtilisateursController"
+            })
+
             .otherwise({
                 redirectTo:"/home"
             })
+
         }]);
+
+app.config(function (paginationTemplateProvider) {
+    paginationTemplateProvider.setPath('libjs/dirPagination.tpl.html');
+});
+
+app.config(["$httpProvider",
+        function ($httpProvider) {
+            // Use x-www-form-urlencoded Content-Type
+            $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            //$httpProvider.defaults.withCredentials = true;
+            /**
+             * The workhorses; converts an object to x-www-form-urlencoded serialization.
+             * @param {Object} obj
+             * @return {String}
+             */
+            var param = function (obj) {
+                var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+                for (name in obj) {
+                    value = obj[name];
+
+                    if (value instanceof Array) {
+                        for (i = 0; i < value.length; ++i) {
+                            subValue = value[i];
+                            fullSubName = name + '[' + i + ']';
+                            innerObj = {};
+                            innerObj[fullSubName] = subValue;
+                            query += param(innerObj) + '&';
+                        }
+                    }
+                    else if (value instanceof Object) {
+                        for (subName in value) {
+                            subValue = value[subName];
+                            fullSubName = name + '[' + subName + ']';
+                            innerObj = {};
+                            innerObj[fullSubName] = subValue;
+                            query += param(innerObj) + '&';
+                        }
+                    }
+                    else if (value !== undefined && value !== null)
+                        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+                }
+
+                return query.length ? query.substr(0, query.length - 1) : query;
+            };
+
+            // Override $http service's default transformRequest
+            $httpProvider.defaults.transformRequest = [function (data) {
+                return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+            }];
+        }
+]);
 
 
--- BusinessWeb/js/controller.js
+++ BusinessWeb/js/controller.js
@@ -1,8 +1,64 @@
 var MainController = angular.module('MainController', []);
 
 //Controleur du GlobalController
-MainController.controller('GlobalController', ['$rootScope', '$scope', '$http',
-function ($rootScope, $scope, $http) {
+MainController.controller('GlobalController', ['$rootScope', '$scope', '$http','$location',
+function ($rootScope, $scope, $http, $location) {
+
+    $rootScope.DateToday = new Date();
+    $scope.user = {};
+
+    $rootScope.Alert = function (type, icon, message) {
+        $scope.alertType = type;
+        $scope.alertIcon = icon;
+        $scope.alertMessage = message;
+        $("#alertbox").fadeIn(1000).delay(3000).fadeOut(1000)
+    }
+
+    $rootScope.AlertIntro = function (type, icon, message) {
+        $rootScope.ialertType = type;
+        $rootScope.ialertIcon = icon;
+        $rootScope.ialertMessage = message;
+        $("#intro-notification").fadeIn(1000).delay(3000).fadeOut(1000)
+    }
+
+    $rootScope.AlertInfo = function (type, icon, message) {
+        $rootScope.infoAlertType = type;
+        $rootScope.infoAlertIcon = icon;
+        $rootScope.infoAlertMessage = message;
+        $("#info-notification").fadeIn(1000).delay(100000).fadeOut(1000)
+    }
+
+    $rootScope.OpenModal = function (element) {
+        $("#" + element).modal('show');
+    }
+    $rootScope.CloseModal = function (element) {
+        $("#" + element).modal('hide');
+    }
+
+    $rootScope.OpenDatePicker = function (input) {
+        ////console.log(input);
+        $("#" + input.target.id).datepicker({
+            dateFormat: "yy-mm-dd"
+        });
+    }
+
+    //$rootScope.ServerURL = "http://10.18.8.58:91/api/";
+    $rootScope.ServerURL = "http://svr-eastest-01/p10/service/api/";
+
+    $scope.isActive = function (route) {
+        return route === $location.path();
+    }
+
+    $rootScope.LISTE_DES_AGENTS = function () {
+        $http.get($rootScope.ServerURL + "Agent")
+       .success(function (response) {
+           $scope.agents = response._responseData;
+           ////console.log(response);
+       });
+    }
+
+    $rootScope.PageName = "";
+    $rootScope.PageDescription = "";
 
     $rootScope.user = {};
     $rootScope.showSidebar = true;
@@ -15,6 +71,9 @@ function ($rootScope, $scope, $http) {
 //Controleur du contenu Principal
 MainController.controller('HomeController', ['$rootScope', '$scope', '$http',
 function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Dashboard";
+    $rootScope.PageDescription = "Vue d'ensemble";
 
     console.log("Home result : ");
     console.log($rootScope.user);
@@ -67,8 +126,111 @@ function ($rootScope, $scope, $http) {
     //$scope.connecter();
 }]);
 
-//Controleur de la page Comptes Clients
-MainController.controller('CompteClientController', ['$rootScope', '$scope', '$http',
+//AchatRapideController
+MainController.controller('AchatRapideController', ['$rootScope', '$scope', '$http',
 function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Achat rapide";
+    $rootScope.PageDescription = "Effectuer un achat rapide";
+
+}]);
+
+//ProduitsController
+MainController.controller('ProduitsController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Produits";
+    $rootScope.PageDescription = "";
+
+}]);
+
+//ClientsController
+MainController.controller('ClientsController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Clients";
+    $rootScope.PageDescription = "Gestion des clients";
+
+}]);
+
+//ChineursController
+MainController.controller('ChineursController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Chineurs";
+    $rootScope.PageDescription = "Gestion des chineurs";
+
+}]);
+
+//FournisseursController
+MainController.controller('FournisseursController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Fournisseurs";
+    $rootScope.PageDescription = "Gestion des fournisseurs";
+
+}]);
+
+//BonLivraisonController
+MainController.controller('BonLivraisonController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Bon de livraison";
+    $rootScope.PageDescription = "";
+
+}]);
+
+//PointJourController
+MainController.controller('PointJourController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Point du jour";
+    $rootScope.PageDescription = "";
+
+}]);
+
+//DepensesController
+MainController.controller('DepensesController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Dépenses";
+    $rootScope.PageDescription = "";
+
+}]);
+
+//AdminProduitsController
+MainController.controller('AdminProduitsController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Produits";
+    $rootScope.PageDescription = "Administration des produits";
+
+
+}]);
+
+//AdminFournisseursController
+MainController.controller('AdminFournisseursController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Fournisseurs";
+    $rootScope.PageDescription = "Administration des fournisseurs";
+
+}]);
+
+//AdminChineursController
+MainController.controller('AdminChineursController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Chineurs";
+    $rootScope.PageDescription = "Administration des Chineurs";
+
+}]);
+
+//AdminUtilisateursController
+MainController.controller('AdminUtilisateursController', ['$rootScope', '$scope', '$http',
+function ($rootScope, $scope, $http) {
+
+    $rootScope.PageName = "Utilisateurs";
+    $rootScope.PageDescription = "Administration des Utilisateurs";
 
 }]);
